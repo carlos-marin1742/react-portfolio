de4188f1d5d2bbdb42aa934bddf6e964ae2f9e42
carlos-marin1742 <95045470+carlos-marin1742@users.noreply.github.com>
2024-08-26 19:48:19 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import styles from "./styles.css";
+import React, { useState } from "react";
+import AboutMe from "./components/AboutMe.js";
+import Projects from "./components/Projects.js";
+
+
+export default function App() {
+  return (
+    <div className={styles.App}>
+
+      <AboutMe />
+      <Projects />
+    </div>
+  );
+}
+
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,22 @@
+import styles from "./Projects.styles.css";
+import React from "react";
+
+const ProjectCard = (props) => {
+  const { project } = props;
+  return (
+    <div class="box">
+      <div class="box-top">
+        <img class="box-image" src={project.imageURL} />
+        <div class="title-flex">
+          <h3 class="box-title">{project.title}</h3>
+        </div>
+        <p class="description">{project.description}</p>
+      </div>
+      <a href="#" src={project.githubLink} class="button">
+        Github Link
+      </a>
+    </div>
+  );
+};
+
+export default ProjectCard;
