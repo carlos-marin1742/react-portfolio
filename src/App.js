f61abc5aec5c10c446704baa3e8a78acb9ae2367
carlos-marin1742 <95045470+carlos-marin1742@users.noreply.github.com>
2024-08-29 19:49:36 -0500

    with updated navbar and contactform

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import styles from "./styles.css";
 import React, { useState } from "react";
 import AboutMe from "./components/AboutMe.js";
 import Projects from "./components/Projects.js";
+import ContactForm from "./components/ContactForm.js";
+import MyNavbar from "./components/MyNavbar.js";
+
+
 
 
 
 export default function App() {
   return (
     <div className={styles.App}>
+      <MyNavbar />
 
       <AboutMe />
       <Projects />
+      < ContactForm />
 
     </div>
   );
