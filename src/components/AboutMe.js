eb051452fc3281f6186bb08acc053d8c9559fdc4
carlos-marin1742 <carlos.a_marin@yahoo.com>
2024-09-19 20:52:00 -0500

    update

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,22 +1,38 @@
-import styles from "./AboutMe.styles.css";
+import "./AboutMe.styles.css";
+import CarlosMarin from "./assets/CarlosMarin.jpeg";
 import React from "react";
-import CarlosMarin from "./assets/CarlosMarin.jpeg"
-import github from "./assets/github.svg"
-import linkedin2 from "./assets/linkedin2.png"
-import github2 from "./assets/GitHub-Logo.png"
-import github3 from "./assets/github3.png"
+import gmail from "./assets/icons8-gmail-400.png";
+import Linkedin from "./assets/icons8-linkedin-400.png";
+import Github from "./assets/icons8-github-400.png";
 
+const AboutMe = () => {
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    textAlign: "center",
+    flexDirection: "column",
+    padding: "20px",
+  };
 
-
-const AboutMe=() =>{
+  const containerStyle2 = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    flexDirection: "row",
+    padding: "20px",
+  };
 
   return (
-    <div id = "BIO">
-      
-      <div className = "image-container">
-        <img className="CarlosMarin" src = {CarlosMarin} alt = "Carlos Marin"/>
-        </div>
-
+    <section style={containerStyle} id="hero">
+      {/* */}
+      {/* My Picture*/}
+      <div classname="hero">
+        <img src={CarlosMarin} alt="picture of me, Carlos Marin" />
+      </div>
+      {/* Name, Title, Icons*/}
       <div classname="about-me-text">
         <h1> Carlos Marin's Portfolio </h1>
         <i>FULL STACK SOFTWARE ENGINEER</i>
@@ -35,23 +51,30 @@ const AboutMe=() =>{
           </i>
           - <strong>Martin Luther King, Jr.</strong>
         </p>
+        {/*ICONS */}
+        <span style={containerStyle2}>
+          {/*GMAIL */}
+          <a href="mailto: carlosmarinjr1@gmail.com" target="_blank">
+            <img src={gmail} alt="gmail icon" className="icons" />
+          </a>
+          {/*LINKEDIN */}
+          <a
+            href="https://www.linkedin.com/in/carlos-marin-90482b13b/"
+            target="_blank"
+            className="icons"
+          >
+            <img src={Linkedin} alt="Linkedin icon" className="icons" />
+          </a>
+          {/*LINKEDIN */}
+          <a
+            href="https://www.linkedin.com/in/carlos-marin-90482b13b/"
+            target="_blank"
+          >
+            <img src={Github} alt="Github icon" className="icons" />
+          </a>
+        </span>
       </div>
-
-            // Test DIV //
-            <div className = "icons">
-        <a className = "LinkedinLink" href = "https://www.linkedin.com/in/carlos-marin-90482b13b/" target="_blank" rel="noopener noreferrer">
-        <img className= "icon" src = {linkedin2} alt = "Linkedin" />
-        </a>
-        <a className = "GithubLink" href = "https://github.com/carlos-marin1742" target="_blank" rel="noopener noreferrer">
-            <img className= "icon" src = {github3} alt = "Github"/>
-            </a>
-        </div>
-
-
-   
-        </div>
-   
+    </section>
   );
 }
-
 export default AboutMe;
