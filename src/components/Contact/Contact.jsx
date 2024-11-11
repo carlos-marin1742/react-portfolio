45f7a84de05bcac8794bc132383f524eb300ff03
carlos-marin1742 <carlos.a_marin@yahoo.com>
2024-11-10 20:30:18 -0600

    Updated Contact Section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="carlosmarinjr1@gmail.com">carlosmarinjr1@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/carlos-marin-90482b13b/">My Linkedin</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://github.com/carlos-marin1742">My Github</a>
         </li>
       </ul>
     </footer>
